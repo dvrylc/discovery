5daf97dcba5695e7b5761a9fb9fd066a96aa33c5
Daryl Chan <dckj95@gmail.com>
2017-11-08 17:52:03 +0800

    Added react router

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
+// External imports
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Internal imports
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 // import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <BrowserRouter>
+
+  </BrowserRouter>,
+  document.getElementById('root')
+);
