cf46127ab735f243741c8e6a36dfabae6dfb3c56
Daryl Chan <dckj95@gmail.com>
2017-11-08 17:59:39 +0800

    Added App component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+// External imports
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="app">
+        Discovery
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 // External imports
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 // Internal imports
-import { BrowserRouter } from 'react-router-dom';
+import App from './components/App';
 import './index.css';
 // import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-
+    <App />
   </BrowserRouter>,
   document.getElementById('root')
 );
