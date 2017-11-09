b14713f6f53f6b8a20f1838dfe4a67e0a76e072f
Daryl Chan <dckj95@gmail.com>
2017-11-09 18:09:18 +0800

    Added timestamps for comments

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -9,6 +9,8 @@ class Comment extends React.Component {
       <div className={ `l${comment.level}` }>
         <p><strong>{ comment.user }</strong></p>
 
+        <time>{ comment.time_ago }</time>
+
         <p dangerouslySetInnerHTML={{ __html: comment.content }} />
       </div>
     );
