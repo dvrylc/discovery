4bd56b12f624fb653a81f3b0005d7f149643a50a
Daryl Chan <dckj95@gmail.com>
2017-11-09 22:48:06 +0800

    Moved initial API call to NewsList - fixes #1

--- src/components/App.js
+++ src/components/App.js
@@ -3,45 +3,20 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 // Internal imports
-import api from '../utilities/api';
 import Header from './Header';
 import About from './About';
 import NewsList from './NewsList';
 import NewsItem from './NewsItem';
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      news: []
-    }
-  }
-
-  componentDidMount() {
-    api.fetchNews()
-      .then(r => r.json())
-      .then(r => {
-        this.setState({
-          news: r
-        });
-
-        console.log('Initial news fetch complete.');
-      })
-      .catch(e => console.error(e));
-  }
-
   render() {
     return (
       <div className="app">
         <Header />
 
-        <Route exact path="/" render={
-          props => <NewsList {...props} news={ this.state.news } />
-        } />
+        <Route exact path="/" component={ NewsList } />
         <Route exact path="/about" component={ About } />
-
-        <Route path="/item/:id" component={ NewsItem } />
+        <Route exact path="/item/:id" component={ NewsItem } />
       </div>
     );
   }
--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -2,15 +2,33 @@
 import React from 'react';
 
 // Internal imports
+import api from '../utilities/api';
 import NewsListItem from './NewsListItem';
 
 class NewsList extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      news: []
+    };
+  }
+
   componentDidMount() {
     document.title = "Discovery";
+
+    api.fetchNews()
+      .then(r => r.json())
+      .then(r => {
+        this.setState({
+          news: r
+        });
+      })
+      .catch(e => console.error(e));
   }
 
   render() {
-    const news = this.props.news;
+    const news = this.state.news;
     const newsListItems = news.map((item, index) => {
       return <NewsListItem key={ index } item={ item } />;
     });
