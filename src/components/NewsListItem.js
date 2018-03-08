675d2960168781000b345ce11c546bf3c072a3f6
Daryl Chan <dckj95@gmail.com>
2018-03-08 18:42:02 +0800

    Added more reliable differentiation of news link types (ask-tell-launch/link/job)

--- src/components/NewsListItem.js
+++ src/components/NewsListItem.js
@@ -7,43 +7,42 @@ class NewsListItem extends React.Component {
     const item = this.props.item;
 
     // Return templates for different items (link / job / ask)
-    switch(item.type) {
-      case 'ask':
-        return (
-          <article className="news-list-item">
+    if (item.type === 'job') {
+      return (
+        <article className="news-list-item">
+          <small className="news-list-item-meta">{ item.time_ago }</small>
+
+          <a href={ item.url }>
+            <h1>{ item.title }</h1>
+          </a>
+        </article>
+      );
+    } else if (item.url.startsWith('item')) {
+      return (
+        <article className="news-list-item">
+          <small className="news-list-item-meta">{ item.user } · { item.time_ago }</small>
+
+          <Link to={{ pathname: `/item/${item.id}` }}>
+            <h1>{ item.title }</h1>
+
+            <small>{ item.points } points · { item.comments_count } comments</small>
+          </Link>
+        </article>
+      );
+    } else {
+      return (
+        <article className="news-list-item">
             <small className="news-list-item-meta">{ item.user } · { item.time_ago }</small>
-
-            <Link to={{ pathname: `/item/${item.id}` }}>
-              <h1>{ item.title }</h1>
-
-              <small>{ item.points } points · { item.comments_count } comments</small>
-            </Link>
-          </article>
-        );
-      case 'job':
-        return (
-          <article className="news-list-item">
-            <small className="news-list-item-meta">{ item.time_ago }</small>
 
             <a href={ item.url }>
               <h1>{ item.title }</h1>
             </a>
-          </article>
-        );
-      default:
-        return (
-          <article className="news-list-item">
-              <small className="news-list-item-meta">{ item.user } · { item.time_ago }</small>
 
-              <a href={ item.url }>
-                <h1>{ item.title }</h1>
-              </a>
-
-            <Link to={{ pathname: `/item/${item.id}` }}>
-              <small>{ item.points } points · { item.comments_count } comments</small>
-            </Link>
-          </article>
-        );
+          <Link to={{ pathname: `/item/${item.id}` }}>
+            <small>{ item.points } points · { item.comments_count } comments</small>
+          </Link>
+        </article>
+      );
     }
   }
 }
